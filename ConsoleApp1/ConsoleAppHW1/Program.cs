ab9b7d72fa1adb0fb8718df84b90140b459ce5b2
Andrii N <42.nazaren.ko@gmail.com>
2022-11-01 17:51:00 +0200

    правки1

--- ConsoleApp1/ConsoleAppHW1/Program.cs
+++ ConsoleApp1/ConsoleAppHW1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Xml.Linq;
+
 Console.WriteLine("what is your name?");
 string name = Console.ReadLine();
 Console.WriteLine("Hello, " +name);
