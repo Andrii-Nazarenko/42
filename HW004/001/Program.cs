add7c6eaafbd896eefa0e66842b8f2544ddad21c
Andrii N <42.nazaren.ko@gmail.com>
2022-11-09 07:43:48 +0200

    урок4. д/з

---
+++ HW004/001/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+Console.WriteLine("введи первое число:");
+string input1 = Console.ReadLine();
+Console.WriteLine("введи второе число:");
+string input2 = Console.ReadLine();
+Console.WriteLine("знак действия:");
+string sign = Console.ReadLine();
+
+double operand1 = int.Parse(input1);
+double operand2 = int.Parse(input2);
+
+
+switch (sign)
+
+{
+    case "+":
+
+        {
+            Console.WriteLine($"результат сложения чисел:{operand1 + operand2}");
+            break;
+        }
+
+    case "-":
+
+        {
+            Console.WriteLine($"результат вычитания чисел:{operand1 - operand2}");
+            break;
+        }
+
+    case "*":
+
+        {
+            Console.WriteLine($"результат умножения чисел:{operand1 * operand2}");
+            break;
+        }
+
+    case "/":
+
+        {
+            if (operand2 != 0)
+            {
+                Console.WriteLine($"результат деления чисел:{operand1 / operand2}");
+            }
+            else 
+            { 
+                Console.WriteLine("на ноль делить нельзя"); 
+            }
+            break;
+        }
+
+    default:
+
+        {
+            Console.WriteLine("я на это пойти не могу");
+            break;
+        }
+
+
+}
