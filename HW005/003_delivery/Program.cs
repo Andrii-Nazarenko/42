68c98baea49e78f361d5ad26939ddf5b0239ed37
Andrii N <42.nazaren.ko@gmail.com>
2022-11-13 18:52:27 +0200

    домашняя работа к уроку 5

---
+++ HW005/003_delivery/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Сколько сегодня доставок? ");
+int ourCliens = int.Parse(Console.ReadLine());
+
+int result = 1;
+
+do
+{
+    result *= ourCliens--;    
+}
+while (ourCliens > 0);
+Console.Write("Количество возможных марщрутов: {0}", result);
